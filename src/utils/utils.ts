6c87e9686480680eced9c27ad9e4efeb932f1ff0
alabsi91 <alabsi91@gmail.com>
2024-06-25 14:58:31 +0300

    Improvements

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -112,7 +112,7 @@ export async function convertEasingFunctionToPoints(
   curves: number[][],
   samples = 1000,
   onUpdate?: (i: number) => void,
-  signal?: AbortSignal
+  signal?: AbortSignal,
 ) {
   let stopped = false;
 
@@ -179,7 +179,8 @@ export function checkOverlap(points: number[][]): boolean {
   return false;
 }
 
-export function throttle(func: (...args: unknown[]) => unknown, delay: number) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function throttle(func: (...args: any[]) => unknown, delay: number) {
   let lastCall = 0;
   return function wrapper(...args: unknown[]) {
     const now = new Date().getTime();
